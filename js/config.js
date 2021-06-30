7a31e4e5e1c36cfffbb5442f7ac7aa2ffd29a101
Raymond Kim <kimraymond9@gmail.com>
2021-06-30 23:08:01 +1200

    change

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ window._config = {
     cognito: {
         userPoolId: 'ap-southeast-2_IodjQvFCC', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '9c9rk395ce4vi3asd7g8pk572', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2' // e.g. us-east-2
+        region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://rjy5xp4lyd.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://nekt385p03.execute-api.ap-southeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
